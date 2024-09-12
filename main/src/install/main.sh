11085350d1632cf6ca00c8c24b25db4cc00c52b8
PrakiRikeki <elias.missal@ribeka.com>
2024-09-12 10:30:28 +0200

    Präziese Ausgaben bei der Installation

--- main/src/install/main.sh
+++ main/src/install/main.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 clear
 
-echo "In den folgenden Schritten werden Docker und andere Komponenten installiert,"
-echo "sodass das Erstellen und Starten eines oder mehrerer SVWS-Server möglich ist."
-
-sleep 2
-
 # Überprüfen, ob das Skript als Root ausgeführt wird
 if [ "$(id -u)" -ne 0 ]; then
     echo "Dieses Skript muss als Root ausgeführt werden." 1>&2
@@ -13,6 +8,8 @@ if [ "$(id -u)" -ne 0 ]; then
     read -n 1 -s -r -p "Drücke irgendeine Taste um fortzufahren..."
     exit 1
 fi
+
+echo "Das Skript wurde als Root ausgeführt"
 
 # Überprüfen, ob die Konfigurationsdatei existiert
 config_file="svws_docker_config.txt"
@@ -24,11 +21,8 @@ if [ ! -f "$config_file" ]; then
     break
 fi
 
-
-
-# Kurze Pause, damit der Benutzer die Nachricht sehen kann
-read -n 1 -s -r -p "Drücke irgendeine Taste um fortzufahren..."
-
+echo "eine config Datei ist vorhanden"
+echo ""
 
 # Funktion zum Einlesen der Konfigurationsdatei
 parse_config() {
@@ -36,6 +30,8 @@ parse_config() {
     local config_file="svws_docker_config.txt"
     local block_found=0
     local line
+
+    echo "config Datei wird eingelesen"
 
     # Einlesen der Konfigurationsdatei
     while IFS= read -r line || [ -n "$line" ]; do
@@ -64,30 +60,60 @@ parse_config() {
             fi
         fi
     done < "$config_file"
-}
 
-# Überprüfen, ob die Konfigurationsdatei existiert
-config_file="svws_docker_config.txt"
-if [[ ! -f "$config_file" ]]; then
-    echo "Fehler: Die Konfigurationsdatei '$config_file' wurde nicht gefunden."
-    break
-fi
+    echo "config Datei wurde erfolgreich eingelesen"
+}
 
 # Liste der Serverblöcke aus der Konfigurationsdatei holen
 server_blocks=$(awk '/^\[.*\]/{gsub(/[\[\]]/,""); print $1}' "$config_file")
 
-    # Docker wird installiert
-    echo "Docker wird installiert"
+    # Systeminstallationsquellenupdate falls vorhanden
+    echo "Installationsquellen werden geprüft."
 
     sudo apt update
-    sudo apt install docker.io docker-compose-v2 nano default-jdk && docker --version
+
+
+    # Updates werden durchgeführt
+    echo "Updates werden durchgeführt."
+
+    sudo apt upgrade
+
+
+    # Docker wird installiert
+    echo "Docker wird installiert oder ein Update wird durchgeführt."
+
+    sudo apt install docker.io
+
+
+    # Docker Compose wird installiert
+    echo "Docker Compose wird installiert, falls nicht vorhanden."
+
+    sudo apt install docker-compose-v2
+    
+
+    # Nano wird geprüft
+    echo "Nano wird geprüft."
+
+    sudo apt install nano 
+
+
+    # Java wird gegebenenfalls installiert, wenn nicht bereits vorhanden
+    echo "Java wird gegebenenfalls installiert, wenn nicht bereits vorhanden."
+    
+    sudo apt install default-jdk
+    
+    
+    # Java wird gegebenenfalls installiert, wenn nicht bereits vorhanden
+    echo "Docker Versions überprüfung."
+    
+    docker --version
 
     clear
 
-    echo "Docker wird installiert"
+    echo "Alle benötigten Programme wurden installiert und auf den neusten Stand gebracht."
 
 
-        # Benutzerabfrage, ob das Skript fortgesetzt werden soll
+    # Benutzerabfrage, ob das Skript fortgesetzt werden soll
     read -p "Wollen Sie fortfahren? [Yn] " response
     response=${response:-y}
 
@@ -97,10 +123,13 @@ server_blocks=$(awk '/^\[.*\]/{gsub(/[\[\]]/,""); print $1}' "$config_file")
       break
     fi
 
+    echo "Installation wird fortgesetzt"
+
 # Schleife über jeden Serverblock
 for server in $server_blocks; do
     clear
-    echo "Verarbeite Konfiguration für: $server"
+    echo "Config wird eingelesen."
+    echo "Verarbeite Konfiguration für: $server."
     echo
     
     # Aufruf der Funktion zur Verarbeitung des Serverblocks
@@ -120,7 +149,7 @@ for server in $server_blocks; do
     echo "SVWS Host Port: ${SVWS_HOST_PORT:-nicht gesetzt}"
     echo
     echo
-
+    echo "Diese Config wird nun verarbeitet."
     sleep 1
 
     clear
@@ -128,19 +157,40 @@ for server in $server_blocks; do
 
 
     #SSL-Zertifikat erstellen
-    echo "SSL-Zertifikat wird erstellt"
+    echo "ein SSL-Zertifikat wird erstellt."
+    echo "eine Sichere Verbindung wird hergestellt." 
 
     # Verzeichnis erstellen, falls nicht vorhanden
     if [ ! -d "$DIR_PATH" ]; then
         echo "Verzeichnis $DIR_PATH existiert nicht. Erstelle es..."
         mkdir -p "$DIR_PATH"
+        echo "Das Verzeichnis $DIR_PATH wurde angelegt."
+    else
+        echo "Das Verzeichnis exestiert bereits."
+        echo "Das vorhande Verzeichnis wird gelöscht."
+        rm -r $DIR_PATH
+        echo "neues Verzeichnis wird angelegt"
+        mkdir -p "$DIR_PATH"
     fi
 
     # Datein erstellen
-    echo "Dateien werden erstellt in $DIR_PATH"
+    echo "Verzeichnis wird betreten."
+    cd $DIR_PATH
+    
+    # Ließ doch einfach den echo Befehl!
+    echo "Serververzeichnis wird erstellt."
+    mkdir svws-server-$ID
 
+    # Arbeitsverzeichnis wird geöffnet
+    echo "Arbeitsverzeichnis wird geöffnet."
+    cd svws-server-$ID
+    
+    # Nun werden die beiden beötigten Daten erstellt
+    echo "benötigte Daten werden generiert."
+    touch docker-compose.yml && touch .env
 
-    cd $DIR_PATH && mkdir svws-server-$ID && cd svws-server-$ID && touch docker-compose.yml && touch .env
+    # Datei wird mit Inhalt beschrieben
+    echo "docker-compose bekommt Inhalt..."
 
     cat <<EOF > docker-compose.yml
     version: "3"
@@ -166,6 +216,10 @@ for server in $server_blocks; do
     
 EOF
 
+    # weitere Terminal ausgeben
+    echo "fertig"
+    echo ".env bekommt Inhalt..."
+
 cat <<EOF > .env
   MariaDB_ROOT_PASSWORD=$MariaDB_ROOT_PASSWORD
   MariaDB_DATABASE=$MariaDB_DATABASE
@@ -177,7 +231,8 @@ cat <<EOF > .env
   SVWS_TLS_KEY_ALIAS=$SVWS_TLS_KEY_ALIAS
 
 EOF
-
+    echo "fertig"
+    echo "Abhänigkeiten werden erstellt."
 
     # Keystore erstellen
     echo "Keystore wird erstellt"
@@ -190,22 +245,28 @@ EOF
 
     # Container starten
     echo
-    echo "Container wird gestartet"
+    echo "Docker Container wird gestartet"
 
-    sleep 1
-
-    clear
-
-    docker compose up -d
-
-    clear
+    docker compose up -d -y 
 
     # Zurück ins Haupt Verzeichnis
     cd ..
     cd ..
 
-    # So sieht dein System jetzt aus
-    echo "########################"
+    echo "Hauptverzeichnis wurde wieder betreten."
+
+    # Statusmeldung wird abgegeben
+
+    gelb="\033[1;33m"
+    rot="\033[1;31m"
+    blau="\033[1;30m"
+    gruen="\033[1;34m"
+    gruen2="\033[1;34m"
+    normal="\033[0;37m"
+
+    echo "$normal Status des Servers:   $gruen gestartet, $gruen2 ja, wirklich! $normal"
+
+
     echo "Der SVWS-Server $server läuft!"
     echo
     echo "Der soeben aufgesetzte Server hat die ID  $ID"
@@ -214,11 +275,10 @@ EOF
     else
     echo "Der Container läuft erfolgreich."
     fi
-    echo "########################"
     echo
     echo
 
-    sleep 2
+    echo "Vorgang wird wiederholt für den nächsten Server"
 
 done
 
